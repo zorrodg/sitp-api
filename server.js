df583bc87d33930510844135aba2246b30ae7b12
Andrés Zorro <zorrodg@gmail.com>
2014-11-23 07:38:45 -0500

    Iniciando servidor Express

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+var app = require('express')();
+
+// Seteando el puerto
+app.set('port', process.env.PORT || 5000);
+
+app.get('/', function (req,res) {
+  res.send('Hello world');
+});
+
+// Init server
+app.listen(app.get('port'), function(){
+  console.log('Servidor express iniciado en puerto %s', app.get('port'));
+});
