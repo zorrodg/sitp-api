250476703f549f69ff6f1aa3c095ced53f003768
Andrés Zorro <zorrodg@gmail.com>
2014-11-23 19:24:01 -0500

    Añadiendo PostgreSQL

--- routes.js
+++ routes.js
@@ -6,12 +6,27 @@
  * @module routes
  */
 
+var pg = require('pg');
+
 function Routes (app) {
   /**
    * Home
    */
   app.get('/', function (req,res) {
     return res.send('Hello stranger, please enter your name in the path');
+  });
+
+  
+  app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err)
+         { console.error(err); response.send("Error " + err); }
+        else
+         { response.send(result.rows); }
+      });
+    });
   });
 
   /**
